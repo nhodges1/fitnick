14701cf5451829a059fb12ec822c0daecc6689df
nhodges <hodges27@outlook.com>
2022-10-02 13:13:52 -0400

    pages and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import './App.css';
+import ExerciseDetail from './Pages/ExerciseDetail';
+import Home from './Pages/Home';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px">
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        </Routes>
+        <Footer />
+    </Box>
   )
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <App />
-)
+);
