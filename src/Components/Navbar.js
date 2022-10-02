7370f11e4edbab7191ff1f72f4d0ba86931dd8c1
nhodges <hodges27@outlook.com>
2022-10-02 16:11:48 -0400

    navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+
+import Logo from '../Assets/images/Logo1.png';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Stack direction="row" justifyContent="space-around" sx={{ gap: {
+      sm: '122px', xs: '40px'}, mt: { sm: 
+      '32px', xs: '20px'}, justifyContent: 'none'}} px="20px">
+      <Link to="/">
+        <img src={Logo} alt="logo" style={{
+          width: '60px', height: '60px', margin: 
+          '0 5px'}} />
+      </Link>
+      <Stack 
+      direction="row"
+      gap="40px"
+      fontSize="24px"
+      alignItems="flex-end"
+      >
+        <Link to="/" style={{ textDecoration:
+        'none', color: "black", 
+        borderBottom: '3px solid black'}}
+        >Home</Link>
+        <a href="#exercises" style={{
+          textDecoration: 'none', color: 'black'}}>Exercises</a>
+      </Stack>
+    </Stack>
   )
 }
 
