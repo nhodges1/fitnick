704a5df3fa82885227d6a059cbae5a46eeae2cb7
nhodges <hodges27@outlook.com>
2022-10-18 20:17:48 -0400

    categories

--- src/Components/HorizontalScrollBar.js
+++ src/Components/HorizontalScrollBar.js
@@ -27,7 +27,7 @@ const RightArrow = () => {
   );
 };
 
-const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
+const HorizontalScrollbar = ({ data, setBodyPart, bodyPart }) => (
   <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
     {data.map((item) => (
       <Box
