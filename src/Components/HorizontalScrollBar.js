f1cab8be07c2b5d2a9a4b74f3163814570639548
nhodges <hodges27@outlook.com>
2022-11-02 20:52:09 -0400

    similar exercises horizontal scrollbar

--- src/Components/HorizontalScrollBar.js
+++ src/Components/HorizontalScrollBar.js
@@ -27,20 +27,25 @@ const RightArrow = () => {
   );
 };
 
-const HorizontalScrollbar = ({ data, setBodyPart, bodyPart }) => (
-  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-    {data.map((item) => (
-      <Box
-        key={item.id || item}
-        itemId={item.id || item}
-        title={item.id || item}
-        m="0 40px"
-      >
-        <BodyPart item={item} bodyPart={bodyPart} setBodyPart=
-        {setBodyPart} />
-      </Box>
-    ))}
-  </ScrollMenu>
-);
+const HorizontalScrollbar = ({ data, setBodyPart, bodyPart, setBodyPart, 
+  isBodyParts }) => {
+    return (
+      <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+        {data.map((item) => (
+          <Box
+            key={item.id || item}
+            itemId={item.id || item}
+            title={item.id || item}
+            m="0 40px"
+          >
+            {isBodyParts ?
+            <BodyPart item={item} bodyPart=
+              {bodyPart} setBodyPart={setBodyPart} />
+            : <ExerciseCard exercise={item} />}
+          </Box>
+        ))}
+      </ScrollMenu>
+    )
+};
 
 export default HorizontalScrollbar;
